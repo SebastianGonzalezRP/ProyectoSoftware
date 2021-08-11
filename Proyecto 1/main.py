139095bd3fa343b4c38b7c61e5eda19bbfb0ca4d
Gustavo Vicuña G <gpvicuna@miuandes.cl>
2021-08-11 17:24:44 -0400

    Update main.py

--- Proyecto 1/main.py
+++ Proyecto 1/main.py
@@ -1,8 +1,22 @@
 from particula import Particula
 
-dt = 0.01
+entry = open("params.txt", "r")
+exit = open("out.txt", "w")
 
-p1 = Particula(0.5, 0.3, 1, 0.2, 0, 3)
-p1.pos()
-p1.tick_move(dt)
-p1.pos()
+t, dt = map(float, entry.readline().split(" "))
+Theta, R, Taus, CL = map(float, entry.readline().split(" "))
+
+while True:
+    try:
+        x0, y0, z0, u0, v0, w0 = map(float, entry.readline().split(" "))
+        p = Particula(x0, y0, z0, u0, v0, w0, Taus)
+        res = p.simulate(t, dt, Theta, R, Taus, CL)
+        for x in res:
+            exit.write(str(x))
+            exit.write(" ")
+        exit.write('\n')
+    except(EOFError):
+        break
+
+entry.close()
+exit.close()
